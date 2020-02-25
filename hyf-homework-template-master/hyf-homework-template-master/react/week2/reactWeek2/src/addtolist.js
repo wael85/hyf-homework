5dda61e052be2b83537bd1734db84e3f5c19c17a
Wael Haded <49494281+wael85@users.noreply.github.com>
2020-02-25 22:01:17 +0100

    Add files via upload

---
+++ hyf-homework-template-master/hyf-homework-template-master/react/week2/reactWeek2/src/addtolist.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+
+export const AddToList = ({ arr }) => {
+  const [arr1, setArr1] = useState(arr);
+
+  const handleClick = () => {
+    const newValue = [...arr1];
+
+    const newRepository = {
+      id: newValue.length,
+      description: "this is very new"
+    };
+
+    newValue.push(newRepository);
+    setArr1(newValue);
+  };
+
+  const HandleDelete = index => {
+    const newValue = arr1;
+    newValue.splice(index, 1);
+
+    setArr1([...newValue]);
+  };
+
+  const AddTo = ({ s, ind }) => {
+     const [ addclass ,setclass] = useState(false);
+     
+
+    return (
+      <div  className = {`${addclass ? `line` : ``}`}>
+        
+        <li>
+          {s}
+          <input type="checkbox" onChange= {()=>setclass(!addclass)}/>
+          <button onClick={() => HandleDelete(ind)}>Delete</button>
+        </li>
+      </div>
+    );
+  };
+
+  return (
+    <div>
+      <button onClick={handleClick}> add</button>
+      <ul className="list">
+        {arr1.map((des, key) => (
+          <AddTo  index={key} s={des.description} />
+        ))}
+      </ul>
+    </div>
+  );
+};
