1d40e94b01f71edd17347b972f67cd815ee18534
Wael Haded <dr.wael85@gmail.com>
2020-05-08 18:08:54 +0200

    promis-visual

---
+++ hyf-homework-template-master/hyf-homework-template-master/javascript/javascript3/week2/class13/Homework/promise-visual/main.js
@@ -0,0 +1,27 @@
+const redBox = document.querySelector('ul.marks li:nth-child(1)');
+const blueBox = document.querySelector('ul.marks li:nth-child(2)');
+const greenBox = document.querySelector('ul.marks li:nth-child(3)');
+const thamb = document.querySelector('span');
+const boxes = [redBox, blueBox, greenBox];
+
+// move one by on
+function translateOneByOne(){
+    moveElement(redBox, {x: 20, y: 300})
+    .then(res=>console.log('red'));
+    setTimeout(()=>{moveElement(blueBox, {x: 400, y: 300})
+    .then(res=>console.log('blue'));},1000);
+    setTimeout(()=>{moveElement(greenBox, {x: 400, y: 20})
+    .then(res=>console.log('green'));},2000);
+    setTimeout(()=>{
+        thamb.classList.add('shown');    
+    },4000)  
+}
+//move all at once
+function translateAllAtOnce(){
+Promise.all([moveElement(redBox, {x: 20, y: 300}),
+    moveElement(blueBox, {x: 400, y: 300}),
+     moveElement(greenBox, {x: 400, y: 20})])
+     .then(()=>console.log('all done'));
+}
+translateOneByOne();
+//translateAllAtOnce();
