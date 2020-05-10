3458581a9db1434f4d1071713cf20058610e1d28
Wael Haded <dr.wael85@gmail.com>
2020-05-10 11:10:27 +0200

    add all feedback comments

--- hyf-homework-template-master/hyf-homework-template-master/javascript/javascript3/week2/class13/Homework/promise-visual/main.js
+++ hyf-homework-template-master/hyf-homework-template-master/javascript/javascript3/week2/class13/Homework/promise-visual/main.js
@@ -7,12 +7,14 @@ const boxes = [redBox, blueBox, greenBox];
 // move one by on
 function translateOneByOne(){
     moveElement(redBox, {x: 20, y: 300})
-    .then(res=>console.log('red'));
-       return moveElement(blueBox, {x: 400, y: 300})
-    .then(res=>console.log('blue'))
-       return moveElement(greenBox, {x: 400, y: 20})
-    .then(res=>console.log('green'))
-    .then(thamb.classList.add('shown'))  
+    .then(()=>{console.log('red');
+       return moveElement(blueBox, {x: 400, y: 300})})
+    .then(()=>{
+       console.log('blue');
+       return moveElement(greenBox, {x: 400, y: 20})})
+    .then(()=>{
+       console.log('green');
+       thamb.classList.add('shown')}); 
 }
 //move all at once
 function translateAllAtOnce(){
