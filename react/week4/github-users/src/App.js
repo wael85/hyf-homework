acc5cd096b9e53d3770949ab46caf0e5cf26f03a
Wael85 <dr.warl85@gmail.com>
2020-08-19 14:48:36 +0200

    reupload

---
+++ react/week4/github-users/src/App.js
@@ -0,0 +1,22 @@
+import React ,{useState }from 'react';
+import './App.css';
+import Search from './SearchPar';
+import RenderUsers from './RenderUsers';
+import {UserContext} from './UserContext';
+
+function App() {
+  const [user , setUser] = useState(null || '');
+  
+  return (
+    <div className="App">
+      <h1>Github user searcher</h1>
+      <UserContext.Provider value = {{user : user}} >
+         <Search  setUser = {setUser}/>  
+         <RenderUsers />
+      </UserContext.Provider >
+      
+    </div>
+  );
+}
+
+export default App;
