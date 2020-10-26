384c98a962b4cce6c2a39cee4e29c6f703c9a90c
Wael85 <dr.warl85@gmail.com>
2020-10-26 10:59:11 +0100

    edit about view

--- react/week5/github-search/src/component/About.jsx
+++ react/week5/github-search/src/component/About.jsx
@@ -2,7 +2,10 @@ import React from 'react';
  export function About (){
      return(
        <div>
-           About
+           <h2>About this App</h2>
+           <p>
+             This app help to search and fetch Github's users and their repos
+           </p>
        </div>
      )
  }
